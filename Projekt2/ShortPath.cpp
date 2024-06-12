604145bb797bd963692c977c9608fdc427595664
Arkadiusz Błasiak <arkbl13@gmail.com>
2024-06-12 19:22:36 +0200

    Bellman-Ford algorithm added

--- Projekt2/ShortPath.cpp
+++ Projekt2/ShortPath.cpp
@@ -10,128 +10,33 @@
 
 using namespace std;
 
-struct Dijkstra{
+struct path{
     int v = -1;
     int d = INT_MAX;
-    int p = NULL;
+    int p = -1;
 };
 
-bool compare_vector2(Dijkstra* a, Dijkstra* b){
+bool compare_vector2(path* a, path* b){
     return (*a).d > (*b).d;
 }
 
 class Compare2 {
     public:
-        bool operator() (Dijkstra a, Dijkstra b){
+        bool operator() (path a, path b){
             return a.d > b.d;
         }
 };
 
-void ShortPah::Dijkstra_Matrix(int ** graph, int edges, int vertices, int start) {
-    /*
-    priority_queue<Dijkstra, vector<Dijkstra>, Compare> queue;
-    priority_queue<Dijkstra, vector<Dijkstra>, Compare> tmp;
-    priority_queue<Dijkstra, vector<Dijkstra>, Compare> tmp2;
-
-    // priority_queue<int, vector<int>, greater<int>> queue;
-    // priority_queue<int, vector<int>, greater<int>> tmp;
-
-    // int ** mst = new int*[vertices];
-    // for (int i = 0; i < vertices; ++i){
-    //     mst[i] = new int[vertices];
-    //     for (int j = 0; j < vertices - 1; ++j){
-    //         mst[i][j] = 0;
-    //     }
-    // }
-
-    int* key = new int[vertices];
-    int* p = new int[vertices];
-
-    Timer timer;
-    for (int i = 0; i < vertices; ++i) {
-        Dijkstra dijkstra;
-        dijkstra.key = INT_MAX;
-        dijkstra.v = i;
-        key[i] = INT_MAX;
-        p[i] = NULL;
-        if (i == 0) {
-            key[i] = 0;
-            dijkstra.key = 0;
-        }
-        queue.push(dijkstra);
-    }
-    
-    bool contains;
-    while (!queue.empty()) {
-        Dijkstra vertex = queue.top();
-        queue.pop();
-        int u = vertex.v;
-
-        for (int e = 0; e < edges; ++e) {
-            if (graph[u][e] > 0) {
-                for (int v = 0; v < vertices; ++v) {
-                    if (graph[v][e] && v != u) {
-
-                        contains = false;
-                        tmp = queue;
-                        while (!tmp.empty()) {
-                        if (tmp.top().v == v && tmp.top().key == key[v]){
-                            // if (tmp.top().v == v){
-                                contains = true;
-                                break;
-                            }
-                            tmp.pop();
-                        }
-
-                        if (contains && graph[v][e] < key[v]) {
-                            priority_queue<Dijkstra, vector<Dijkstra>, Compare> tmp2;
-                            while (!(queue.top().v == v && queue.top().key == key[v])) {
-                                tmp2.push(queue.top());
-                                queue.pop();
-                            }
-                            key[v] = graph[v][e];
-                            p[v] = u;
-                            Dijkstra dijkstra = queue.top();
-                            queue.pop();
-                            dijkstra.key = graph[v][e];
-                            queue.push(prim);
-                            while (!tmp2.empty()) {
-                                queue.push(tmp2.top());
-                                tmp2.pop();
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    float time = timer.stop();
-
-    cout << "time Dijkstra's mst alg = " << time << endl;
-    // int edge = 0;
-    // for (int i = 1; i < vertices; ++i){
-    //     if (type == Generate_graph::graph_type::directed) {
-    //         mst[i][edge] = key[i] > 0 ? -key[i] : key[i];
-    //         mst[p[i]][edge] = key[i] > 0 ? key[i] : -key[i];
-    //     }
-    //     else if (type == Generate_graph::graph_type::undirected) {
-    //         mst[i][edge] = key[i];
-    //         mst[p[i]][edge] = key[i];
-    //     }
-    //     ++edge;
-    */
-
-
+void ShortPath::Dijkstra_Matrix(int ** graph, int edges, int vertices, int start, int stop) {
     if (start >= vertices){
         cout << "nieprawidlowa liczba!" << endl;
         return;
     }
 
-
     Timer timer;
 
-    vector<Dijkstra*> queue;
-    Dijkstra* dijkstras = new Dijkstra[vertices];
+    vector<path*> queue;
+    path* dijkstras = new path[vertices];
 
     for (int i = 0; i < vertices; ++i) {
         dijkstras[i].v = i;
@@ -142,7 +47,7 @@ void ShortPah::Dijkstra_Matrix(int ** graph, int edges, int vertices, int start)
 
     while (!queue.empty()) {
     sort(queue.begin(), queue.end(), compare_vector2);
-        Dijkstra* vertex = queue.back();
+        path* vertex = queue.back();
         queue.pop_back();
         int u = (*vertex).v;
 
@@ -150,7 +55,7 @@ void ShortPah::Dijkstra_Matrix(int ** graph, int edges, int vertices, int start)
             if (graph[u][e] > 0) {
                 for (int v = 0; v < vertices; ++v) {
                     if (graph[v][e] && v != u) {
-                        if (graph[u][e] + dijkstras[u].d < dijkstras[v].d) {
+                        if (graph[u][e] + dijkstras[u].d < dijkstras[v].d && graph[u][e] + dijkstras[u].d >= 0) {
                             dijkstras[v].d = graph[u][e] + dijkstras[u].d;
                             dijkstras[v].p = u;
                         }
@@ -163,6 +68,13 @@ void ShortPah::Dijkstra_Matrix(int ** graph, int edges, int vertices, int start)
     float time = timer.stop();
 
     cout << "Dijkstra matrix time = " << time << "ms" << endl;
+    int pre = dijkstras[stop].p;
+    cout << stop;
+    while (pre != -1) {
+        cout << " <- " << pre;
+        pre = dijkstras[pre].p;
+    }
+    cout << endl << "total cost = " << dijkstras[stop].d << endl;
     // cout << "from 0 to whatever you want\n";
     // for (int i = 0; i < vertices; ++i){
     //     cout << "to " << i << " from " << dijkstras[i].p << " length " << dijkstras[i].d << endl;
@@ -170,20 +82,20 @@ void ShortPah::Dijkstra_Matrix(int ** graph, int edges, int vertices, int start)
 
     delete [] dijkstras;
     queue.clear();
-    delete &queue;
+    // delete &queue;
 }
 
-void ShortPah::Dijkstra_List(Graph_list ** graph, int edges, int vertices, int start) {
+void ShortPath::Dijkstra_List(Graph_list ** graph, int edges, int vertices, int start, int stop) {
+
         if (start >= vertices){
         cout << "nieprawidlowa liczba!" << endl;
         return;
     }
 
-
     Timer timer;
 
-    vector<Dijkstra*> queue;
-    Dijkstra* dijkstras = new Dijkstra[vertices];
+    vector<path*> queue;
+    path* dijkstras = new path[vertices];
 
     for (int i = 0; i < vertices; ++i) {
         dijkstras[i].v = i;
@@ -194,7 +106,7 @@ void ShortPah::Dijkstra_List(Graph_list ** graph, int edges, int vertices, int s
 
     while (!queue.empty()) {
         sort(queue.begin(), queue.end(), compare_vector2);
-        Dijkstra* vertex = queue.back();
+        path* vertex = queue.back();
         queue.pop_back();
         int u = (*vertex).v;
 
@@ -202,7 +114,7 @@ void ShortPah::Dijkstra_List(Graph_list ** graph, int edges, int vertices, int s
         while(l) {
             int v = l->v;
             int w = l->w;
-            if (w > 0 && w + dijkstras[u].d < dijkstras[v].d) {
+            if (w > 0 && w + dijkstras[u].d < dijkstras[v].d && w + dijkstras[u].d >= 0) {
                 dijkstras[v].d = w + dijkstras[u].d;
                 dijkstras[v].p = u;
             }
@@ -213,6 +125,13 @@ void ShortPah::Dijkstra_List(Graph_list ** graph, int edges, int vertices, int s
     float time = timer.stop();
 
     cout << "Dijkstra list time = " << time << "ms" << endl;
+    int pre = dijkstras[stop].p;
+    cout << stop;
+    while (pre != -1) {
+        cout << " <- " << pre;
+        pre = dijkstras[pre].p;
+    }
+    cout << endl << "total cost = " << dijkstras[stop].d << endl;
     // cout << "from 0 to whatever you want\n";
     // for (int i = 0; i < vertices; ++i){
     //     cout << "to " << i << " from " << dijkstras[i].p << " length " << dijkstras[i].d << endl;
@@ -220,5 +139,132 @@ void ShortPah::Dijkstra_List(Graph_list ** graph, int edges, int vertices, int s
 
     delete [] dijkstras;
     queue.clear();
-    delete &queue;
+    // delete &queue;
+}
+
+void ShortPath::BF_Matrix(int ** graph, int edges, int vertices, int start, int stop) {
+
+    Timer timer;
+
+    path* BFs = new path[vertices];
+    int * d = new int[vertices];
+    int * p = new int[vertices];
+    for (int i = 0; i < vertices; ++i) {
+        d[i] = INT_MAX;
+        p[i] = -1;
+    }
+    d[start] = 0;
+
+    bool relaxation = true;
+    for (int i = 0; i < vertices - 1 && relaxation; ++i) {
+        relaxation = false;
+    for (int u = 0; u < vertices; ++u) {
+        for (int e = 0; e < edges; ++e) {
+            if (graph[u][e] > 0) {
+                for (int v = 0; v < vertices; ++v) {
+                    if (graph[v][e] < 0 && v != u ){
+                        if (d[v] > d[u] + graph[u][e] && d[u] + graph[u][e] >= 0) {
+                        d[v] = d[u] + graph[u][e];
+                        p[v] = u;
+                        relaxation = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    }
+    for (int u = 0; u < vertices; ++u) {
+        for (int e = 0; e < edges; ++e) {
+            if (graph[u][e] > 0) {
+                for (int v = 0; v < vertices; ++v) {
+                    if (graph[v][e] < 0 && d[v] > d[u] + graph[u][e] && d[u] + graph[u][e] >= 0) {
+                        cout << "Cykl ujemny!!!\n";
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    float time = timer.stop();
+    cout << "BF matrix time = " << time << " ms" << endl;
+
+    // cout << "from 0 to whatever you want\n";
+    // for (int i = 0; i < vertices; ++i){
+    //     cout << "to " << i << " from " << p[i] << " length " << d[i] << endl;
+    // }
+    int pre = p[stop];
+    cout << stop;
+    while (pre != -1) {
+        cout << " <- " << pre;
+        pre = p[pre];
+    }
+    cout << endl << "total cost = " << d[stop] << endl;
+
+    delete [] d;
+    delete [] p;
+    delete [] BFs;
+
+}
+
+void ShortPath::BF_List(Graph_list** graph, int edges, int vertices, int start, int stop) {
+
+    Timer timer;
+
+    path* BFs = new path[vertices];
+    int * d = new int[vertices];
+    int * p = new int[vertices];
+    for (int i = 0; i < vertices; ++i) {
+        d[i] = INT_MAX;
+        p[i] = -1;
+    }
+    d[start] = 0;
+
+    bool relaxation = true;
+    for (int i = 0; i < vertices - 1 && relaxation; ++i) {
+        relaxation = false;
+    for (int u = 0; u < vertices; ++u) {
+        Graph_list *l = graph[u];
+        while(l) {
+            int v = l->v;
+            int w = l->w;
+            if (w > 0 && d[v] > d[u] + w && d[u] + w >= 0) {
+                d[v] = d[u] + w;
+                p[v] = u;
+                relaxation = true;
+            }
+            l = l->next;
+        }
+    }
+    }
+    for (int u = 0; u < vertices; ++u) {
+        Graph_list *l = graph[u];
+        while(l) {
+            int v = l->v;
+            int w = l->w;
+            if (w > 0 && d[v] > d[u] + w && d[u] + w >= 0) {
+                cout << "Cykl ujemny!!!\n";
+                return;
+            }
+            l = l->next;
+        }
+    }
+    float time = timer.stop();
+    cout << "BF list time = " << time << " ms" << endl;
+
+    // cout << "from 0 to whatever you want\n";
+    // for (int i = 0; i < vertices; ++i){
+    //     cout << "to " << i << " from " << p[i] << " length " << d[i] << endl;
+    // }
+    int pre = p[stop];
+    cout << stop;
+    while (pre != -1) {
+        cout << " <- " << pre;
+        pre = p[pre];
+    }
+    cout << endl << "total cost = " << d[stop] << endl;
+
+    delete [] d;
+    delete [] p;
+    delete [] BFs;
 }
--- Projekt2/ShortPath.h
+++ Projekt2/ShortPath.h
@@ -3,10 +3,12 @@
 
 #include "graph_list.h"
 
-class ShortPah{
+class ShortPath{
     public:
-        void Dijkstra_Matrix(int ** graph, int e, int v, int start);
-        void Dijkstra_List(Graph_list ** graph, int edges, int vertices, int start);
+        void Dijkstra_Matrix(int ** graph, int e, int v, int start, int stop);
+        void Dijkstra_List(Graph_list ** graph, int edges, int vertices, int start, int stop);
+        void BF_Matrix(int ** graph, int e, int v, int start, int stop);
+        void BF_List(Graph_list ** graph, int e, int v, int start, int stop);
 };
 
 #endif
